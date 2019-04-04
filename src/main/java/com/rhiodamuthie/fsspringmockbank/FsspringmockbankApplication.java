ebc54461fb5656b4c195552194d277cd829624a2
Rhioda muthie <rhioda.m@gmail.com>
2019-04-05 05:05:10 +0700

    Initial commit

---
+++ src/main/java/com/rhiodamuthie/fsspringmockbank/FsspringmockbankApplication.java
@@ -0,0 +1,13 @@
+package com.rhiodamuthie.fsspringmockbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FsspringmockbankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FsspringmockbankApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rhiodamuthie/fsspringmockbank/FsspringmockbankApplicationTests.java
@@ -0,0 +1,16 @@
+package com.rhiodamuthie.fsspringmockbank;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FsspringmockbankApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
